5298b78bc18b097b7bc666960368f2f312cd12a4
Unknown <cwhite788@gmail.com>
2016-09-16 11:57:25 +0100

    Added temporary icons to map

--- www/js/controllers/MapController.js
+++ www/js/controllers/MapController.js
@@ -20,4 +20,33 @@ trottr.controller('MapController', function($scope){
 					});
           map.setView(new L.LatLng(51.7711375, 19.4732066),15);
         	map.addLayer(osm);
+  var PoIIcon = L.Icon.extend({
+	       options: {
+          			shadowUrl: '../img/icon-shadow.png',
+          			iconSize:     [60, 40],
+          	    shadowSize:   [50, 64],
+          	    iconAnchor:   [22, 94],
+          	    shadowAnchor: [4, 62],
+          	    popupAnchor:  [-3, -76]
+          		}
+          	});
+      	var museumIcon = new PoIIcon({iconUrl:"../img/museums.png"}),
+        parkIcon = new PoIIcon({iconUrl:"../img/parks.png"}),
+        jewishIcon = new PoIIcon({iconUrl:"../img/jewish.png"}),
+        christianIcon = new PoIIcon({iconUrl:"../img/christian.png"}),
+        sportsIcon = new PoIIcon({iconUrl:"../img/sports.png"}),
+        monumentsIcon = new PoIIcon({iconUrl:"../img/monuments.png"}),
+        foodIcon = new PoIIcon({iconUrl:"../img/food.png"}),
+        entertainmentIcon = new PoIIcon({iconUrl:"../img/entertainment.png"})
+        drinkIcon = new PoIIcon({iconUrl:"../img/drinks.png"});
+
+        L.marker([51.771791, 19.478403], {icon: museumIcon}).addTo(map);
+        L.marker([51.773472, 19.476705], {icon: parkIcon}).addTo(map);
+        L.marker([51.775422, 19.461213], {icon: jewishIcon}).addTo(map);
+        L.marker([51.774771, 19.481104], {icon: christianIcon}).addTo(map);
+        L.marker([51.781408, 19.457570], {icon: sportsIcon}).addTo(map);
+        L.marker([51.776790, 19.454710], {icon: monumentsIcon}).addTo(map);
+        L.marker([51.779498, 19.482959], {icon: foodIcon}).addTo(map);
+        L.marker([51.780841, 19.448379], {icon: entertainmentIcon}).addTo(map);
+        L.marker([51.771252, 19.468391], {icon: drinkIcon}).addTo(map);
 });
