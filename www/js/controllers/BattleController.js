753630c71ce0a7d5fff47b60d7d84f9c783eedb1
Kieran Hoey <Kieranhoey1995@gmail.com>
2016-09-19 15:52:34 +0100

    Added skeleton angular functions

--- www/js/controllers/BattleController.js
+++ www/js/controllers/BattleController.js
@@ -1,5 +1,14 @@
-trottr.controller('BattleController', function($scope)
-{
-  var canvas = angular.element(document.querySelector('#battle-canvas')),
-  var context = canvas.getContext('2d'),
+trottr.controller('BattleController', function($scope){
+  var canvas = angular.element(document.querySelector('#battle-canvas'))[0];
+  var context = canvas.getContext('2d');
+
+
+  shield.on('click', function(){
+
+  });
+
+  weapon.on('click', function(){
+      
+  });
+
 });
