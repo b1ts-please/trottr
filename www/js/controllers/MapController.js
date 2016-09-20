6e0793eeca01d0570b5fc3a647c1776d562dffce
Unknown <cwhite788@gmail.com>
2016-09-20 16:24:43 +0100

    Added left and right touch controls

--- www/js/controllers/MapController.js
+++ www/js/controllers/MapController.js
@@ -1,38 +1,37 @@
 trottr.controller('MapController', function($scope){
-  var map;
   var ajaxRequest;
   var plotlist;
   var plotlayers = [];
 
-  map = new L.Map('map',{zoomControl:false});
+  var map = new L.Map('map',{zoomControl:false});
 
-	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-	var osm = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-		attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-		subdomains: 'abcd',
-		minZoom: 15,
-		maxZoom: 16,
-		touchZoom: false,
-		ext: 'png'
-	});
+  // create the tile layer with correct attribution
+  var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+  var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+  var osm = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: 'abcd',
+    minZoom: 15,
+    maxZoom: 16,
+    touchZoom: false,
+    ext: 'png'
+  });
   map.setView(new L.LatLng(51.7711375, 19.4732066),15);
-	map.addLayer(osm);
+  map.addLayer(osm);
 
   // Create icons
   var PoIIcon = L.Icon.extend({
     options: {
-			shadowUrl: 'img/shadow.png',
-			iconSize:     [110, 110],
-	    shadowSize:   [100, 100],
-	    iconAnchor:   [45, 30],
-	    shadowAnchor: [15, 60],
-	    popupAnchor:  [-3, -76]
-		}
-	});
+      shadowUrl: 'img/shadow.png',
+      iconSize:     [110, 110],
+      shadowSize:   [100, 100],
+      iconAnchor:   [45, 30],
+      shadowAnchor: [15, 60],
+      popupAnchor:  [-3, -76]
+    }
+  });
 
-	var museumIcon = new PoIIcon({iconUrl:"img/museums.png"});
+  var museumIcon = new PoIIcon({iconUrl:"img/museums.png"});
   var parkIcon = new PoIIcon({iconUrl:"img/parks.png"});
   var jewishIcon = new PoIIcon({iconUrl:"img/jewish.png"});
   var christianIcon = new PoIIcon({iconUrl:"img/christian.png"});
@@ -69,5 +68,8 @@ trottr.controller('MapController', function($scope){
     angular.element(document.querySelector('#ghost-map .ghost-head h2')).text(head);
     angular.element(document.querySelector('#ghost-map .ghost-body')).text(body);
     angular.element(document.querySelector('#ghost-map')).css('display', 'block');
+  }
+  $scope.relocate = function(){
+    this.map.setView(new L.LatLng(51.7711375, 19.4732066),15);
   }
 });
