ef17e24e7c4a54ac5390fdb7081b5c2bff9ec642
Dominik <domin1204@gmail.com>
2016-09-21 16:51:40 +0200

    First working version of battle

--- www/js/services/PlayerService.js
+++ www/js/services/PlayerService.js
@@ -1,21 +1,39 @@
 trottr.service('PlayerService', function(){
   this.playerHealth = 0;
   this.playerLevel = 0;
+  this.playerDefending = false;
+  this.playerMaxHealth = 0;
 
   this.setPlayerHealth = function(playerHealth){
     this.playerHealth = playerHealth;
   }
 
-  this.getplayerHealth = function(){
+  this.getPlayerHealth = function(){
     return this.playerHealth;
   }
 
-  this.setPlayerLevel = function(playerlevel){
-    this.playerLevel = playerlevel;
+  this.setPlayerLevel = function(playerLevel){
+    this.playerLevel = playerLevel;
   }
 
   this.getPlayerLevel = function(){
     return this.playerLevel;
+  }
+
+  this.setPlayerDefending = function(playerDefending){
+    this.playerDefending = playerDefending;
+  }
+
+  this.getPlayerDefending = function(){
+    return this.playerDefending;
+  }
+
+  this.setPlayerMaxHealth = function(playerMaxHealth){
+    this.playerMaxHealth = playerMaxHealth;
+  }
+
+  this.getPlayerMaxHealth = function(){
+    return this.playerMaxHealth;
   }
 
 });
